951f851a7d70b3edb52093d50f742e1541e98b2c
maurilio <mauriliojose249@gmail.com>
2022-12-07 10:45:20 -0300

    Implementação da data e hora em tempo real na base inferior direita da calculadora e início da implementação do botão de copiar o display para colar posteriormente em outro ambiente.

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton, QLineEdit, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, \
+    QPushButton, QLineEdit, QSizePolicy, QLabel
+from datetime import datetime
 
 
 class Calculadora(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        data_e_hora = datetime.now()
+        data_e_hora = data_e_hora.strftime('%A %d %B %y %I:%M')
         self.setWindowTitle('Calculadora')
-        self.setFixedSize(450, 450)
+        self.setFixedSize(500, 600)
         self.cw = QWidget()
         self.grid = QGridLayout(self.cw)
         self.display = QLineEdit()
-        self.grid.addWidget(self.display, 0, 0, 1, 5)
-        self.display.setDisabled(True)
+        self.grid.addWidget(self.display, 0, 0, 1, 4)
+        self.display.setDisabled(False)
         self.display.setStyleSheet(
             '* {background: white; color black; font-size: 30px;}'
         )
         self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
+        self.copia(QPushButton('Copiar'), 0, 4, 1, 1)
+
+        self.data_e_hora(QLabel(data_e_hora), 5, 3, 1, 2)
+
         self.addBotoes(QPushButton('7'), 1, 0, 1, 1)
         self.addBotoes(QPushButton('8'), 1, 1, 1, 1)
         self.addBotoes(QPushButton('9'), 1, 2, 1, 1)
         self.addBotoes(QPushButton('<-'), 1, 3, 1, 1, lambda: self.display.setText(self.display.text()[:-1]),
-                                                        'background: orange; color: white; font-wheight: 700;')
+                       'background: orange; color: white; font-wheight: 700;')
         self.addBotoes(QPushButton('C'), 1, 4, 1, 1, lambda: self.display.setText(''), 'background: red; color: white;'
                                                                                        ' font-wheight: 700;')
         self.addBotoes(QPushButton('4'), 2, 0, 1, 1)
@@ -39,6 +47,14 @@ class Calculadora(QMainWindow):
         self.addBotoes(QPushButton('='), 3, 4, 2, 1, self.igual, 'background: blue; color: white; font-wheigth: 700')
 
         self.setCentralWidget(self.cw)
+
+    def copia(self, btn, row, col, rowspan, colspan, funcao=None):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
+        if funcao:
+            btn.clicked.connect(lambda: self.display.copy())
+
+    def data_e_hora(self, btn, row, col, rowspan, colspan):
+        self.grid.addWidget(btn, row, col, rowspan, colspan)
 
     def addBotoes(self, btn, row, col, rowspan, colspan, funcao=None, style=None):
         self.grid.addWidget(btn, row, col, rowspan, colspan)
